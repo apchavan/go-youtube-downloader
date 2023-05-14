80f247450e3e43004778d23b29fba515a3b39008
Amey Chavan <ameyc96@gmail.com>
2023-05-14 22:12:47 +0530

    Added more string getter functions

--- runner/constants.go
+++ runner/constants.go
@@ -12,9 +12,14 @@ func GetVideoLinkInputFieldLabel() string {
 	return "Paste/Type YouTube Video Link : "
 }
 
-// Return label for `Video Quality` dropdown.
-func GetVideoQualityDropdownLabel() string {
-	return "Select Video Quality : "
+// Return label for video related dropdown.
+func GetVideoQuality_FPS_Size_Type_DropdownLabel() string {
+	return "Video Quality, FPS, Size & Type : "
+}
+
+// Return label for audio related dropdown.
+func GetAudioBitrate_Size_Type_DropdownLabel() string {
+	return "Audio Bitrate, Size & Type : "
 }
 
 // Return label for Download button.
@@ -42,9 +47,14 @@ func GetDownloadingMessage(downloadingVideoTitle string) string {
 	return fmt.Sprintf("Downloading '%s'...", downloadingVideoTitle)
 }
 
+// Return status message for Invalid Video URL.
+func GetInvalidURLMessage(videoURL string) string {
+	return fmt.Sprintf("No YouTube video found at :> '%s'", videoURL)
+}
+
 // Return status message for Download Finished.
-func GetDownloadFinishedMessage() string {
-	return "Download Finished!"
+func GetDownloadFinishedMessage(downloadingVideoTitle string) string {
+	return fmt.Sprintf("Download Finished for '%s'!", downloadingVideoTitle)
 }
 
 // Returns the string of info title.
@@ -69,15 +79,18 @@ func GetUsageInfoLabel() string {
 
 // Return string text for app usage info.
 func GetUsageInfoText() string {
-	return "Paste/type the YouTube video link in above input field. Then click on appeared 'Download' button."
+	var line1 string = "(1) Paste/type the YouTube video link in above input field.\n"
+	var line2 string = "(2) Select the required 'Video Quality, FPS, Size & Type' & 'Audio Bitrate, Size & Type' from their dropdowns.\n"
+	var line3 string = "(3) Then click on newly appeared 'Download' button."
+	return (line1 + line2 + line3)
 }
 
 // Return label for Quit info.
 func GetQuitInfoLabel() string {
-	return "How to quit ? : "
+	return "Quit app using : "
 }
 
 // Return string text for Quit info.
 func GetQuitInfoText() string {
-	return "Press keyboard shortcut <Ctrl + c>"
+	return "Keyboard shortcut '<Ctrl> + c'"
 }
