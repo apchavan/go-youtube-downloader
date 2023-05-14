b6d206dfbcae7939a437f26d60d59855c1cbcadb
Amey Chavan <ameyc96@gmail.com>
2023-05-14 22:11:26 +0530

    Updated terminal UI & logic

--- runner/tui.go
+++ runner/tui.go
@@ -6,8 +6,8 @@ import (
 
 // Construct & return the application instance.
 func GetTuiAppLayout() *tview.Application {
-	// Create pointer object of `YouTubeVideoOperations` struct
-	youtubeVideoOperations := &YouTubeVideoOperations{VideoUrl: "", VideoQuality: ""}
+	// Create pointer object of `YouTubeVideoDetailsStruct` struct
+	youtubeVideoDetails := &YouTubeVideoDetailsStruct{}
 
 	// Create application object
 	application := tview.NewApplication()
@@ -16,10 +16,10 @@ func GetTuiAppLayout() *tview.Application {
 	flexLayout := tview.NewFlex()
 
 	// Get input form
-	inputForm := GetInputForm(application, youtubeVideoOperations)
+	inputForm := getInputForm(application, youtubeVideoDetails)
 
 	// Get info form
-	infoForm := GetInfoForm(application, youtubeVideoOperations)
+	infoForm := getInfoForm()
 
 	// Add both forms to the `flexLayout`
 	flexLayout = flexLayout.AddItem(
@@ -36,26 +36,71 @@ func GetTuiAppLayout() *tview.Application {
 }
 
 // Create & return input form
-func GetInputForm(application *tview.Application, youtubeVideoOperations *YouTubeVideoOperations) *tview.Form {
+func getInputForm(application *tview.Application, youtubeVideoDetails *YouTubeVideoDetailsStruct) *tview.Form {
 	inputForm := tview.NewForm()
 
 	inputForm = inputForm.AddInputField(
 		GetVideoLinkInputFieldLabel(), "", 0, nil,
-		func(text string) {
-			youtubeVideoOperations.VideoUrl = text
+		func(urlText string) {
+			youtubeVideoDetails.VideoUrl = urlText
 
-			// TODO: Fetch, check video's metadata & set the quality options array of string if video is valid
-			if text != "" && inputForm.GetFormItemIndex(GetVideoQualityDropdownLabel()) == -1 {
-				// Create the Video Quality dropdown when proper link text is present in the input field
+			// TODO: Fetch, check video's metadata & set the quality & FPS options array of string if video is valid
+			if urlText != "" {
+				if !youtubeVideoDetails.IsValidYouTubeURL() {
+					// Clear controls if already exist
+					removeControls(inputForm, true, true, true, true)
+
+					// TODO: Pass download finished message with video title
+					inputForm = inputForm.AddTextView(GetStatusLabel(),
+						GetInvalidURLMessage(youtubeVideoDetails.VideoUrl),
+						0, 0, true, true)
+					return
+				} else {
+					// Clear previous status textviews if found any
+					removeControls(inputForm, false, false, true, false)
+				}
+
+				// Create the Video dropdown when proper link text is present in the input field
 				// and no existing input dropdown is already exist.
-				inputForm = inputForm.AddDropDown(
-					GetVideoQualityDropdownLabel(),
-					[]string{"144p", "240p", "480p", "720p", "1080p"},
-					0,
+				// TODO: Pass actual video qualities highest first, lowest last
+				if inputForm.GetFormItemIndex(GetVideoQuality_FPS_Size_Type_DropdownLabel()) == -1 {
+					inputForm = inputForm.AddDropDown(
+						GetVideoQuality_FPS_Size_Type_DropdownLabel(),
+						[]string{
+							" 1440p60 | 60 fps | 68.6 MB | MP4 ",
+							" 1080p60 HDR | 60 fps | 58.6 MB | WEBM ",
+							" 720p60 | 60 fps | 48.6 MB | MP4 ",
+							" 480p | 30 fps | 38.6 MB | WEBM ",
+							" 360p | 24 fps | 28.6 MB | MP4 ",
+							" 144p | 24 fps | 18.6 MB | WEBM ",
+						},
+						0,
 
-					func(option string, optionIndex int) {
-						youtubeVideoOperations.VideoQuality = option
-					})
+						func(option string, optionIndex int) {
+							youtubeVideoDetails.SelectedVideoQuality = option
+						})
+				}
+
+				// Create the Audio dropdown when proper link text is present in the input field
+				// and no existing input dropdown is already exist.
+				// TODO: Pass actual audio qualities highest first, lowest last
+				if inputForm.GetFormItemIndex(GetAudioBitrate_Size_Type_DropdownLabel()) == -1 {
+					inputForm = inputForm.AddDropDown(
+						GetAudioBitrate_Size_Type_DropdownLabel(),
+						[]string{
+							" 631073 | 68.6 MB | MP4 ",
+							" 531073 | 58.6 MB | WEBM ",
+							" 431073 | 48.6 MB | MP4 ",
+							" 331073 | 38.6 MB | WEBM ",
+							" 231073 | 28.6 MB | MP4 ",
+							" 131073 | 18.6 MB | WEBM ",
+						},
+						0,
+
+						func(option string, optionIndex int) {
+							youtubeVideoDetails.SelectedAudioQuality = option
+						})
+				}
 
 				// Add `Download` button if already not present.
 				if inputForm.GetButtonIndex(GetDownloadButtonLabel()) == -1 {
@@ -63,40 +108,43 @@ func GetInputForm(application *tview.Application, youtubeVideoOperations *YouTub
 						GetDownloadButtonLabel(),
 
 						func() {
-							if youtubeVideoOperations.VideoUrl != "" && youtubeVideoOperations.VideoQuality != "" {
+							if youtubeVideoDetails.VideoUrl != "" &&
+								youtubeVideoDetails.SelectedVideoQuality != "" &&
+								youtubeVideoDetails.SelectedAudioQuality != "" {
 								// Clear previous status textviews if found any
-								for inputForm.GetFormItemIndex(GetStatusLabel()) != -1 {
-									inputForm.RemoveFormItem(inputForm.GetFormItemIndex(GetStatusLabel()))
-								}
+								removeControls(inputForm, false, false, true, false)
 
 								// Before starting download, change the label & set button disabled
 								inputForm.GetButton(inputForm.GetButtonIndex(GetDownloadButtonLabel())).
 									SetLabel(GetDownloadButtonProgressLabel()).SetDisabled(true)
 
 								// Add & set textview to show download in progress...
+								// TODO: Pass downloading message with video title
 								inputForm = inputForm.AddTextView(GetStatusLabel(),
-									GetDownloadingMessage(youtubeVideoOperations.VideoUrl),
+									GetDownloadingMessage(youtubeVideoDetails.VideoUrl),
 									0, 0, true, true)
 
 								// Very important to re-draw the screen before actually starting the video download
 								application.ForceDraw().Sync()
 
 								// TODO: Handle download action
-								downloadStatusChannel := make(chan bool)
-								go youtubeVideoOperations.DownloadYouTubeVideo(downloadStatusChannel)
+								downloadStateChannel := make(chan bool)
+								go youtubeVideoDetails.DownloadYouTubeVideo(downloadStateChannel)
 
-								downloadStatus := false
-								for value := range downloadStatusChannel {
-									downloadStatus = value
+								// Check the status whether download has finished
+								isDownloadFinished := false
+								for value := range downloadStateChannel {
+									isDownloadFinished = value
 								}
 
 								// Change textview to show download finished...
-								if downloadStatus {
+								if isDownloadFinished {
 									// Clear previous status textviews if found any
-									for inputForm.GetFormItemIndex(GetStatusLabel()) != -1 {
-										inputForm.RemoveFormItem(inputForm.GetFormItemIndex(GetStatusLabel()))
-									}
-									inputForm = inputForm.AddTextView(GetStatusLabel(), GetDownloadFinishedMessage(),
+									removeControls(inputForm, false, false, true, false)
+
+									// TODO: Pass download finished message with video title
+									inputForm = inputForm.AddTextView(GetStatusLabel(),
+										GetDownloadFinishedMessage(youtubeVideoDetails.VideoUrl),
 										0, 0, true, true)
 
 									// In the end, reset the download button.
@@ -107,20 +155,9 @@ func GetInputForm(application *tview.Application, youtubeVideoOperations *YouTub
 							}
 						}).SetButtonsAlign(tview.AlignRight)
 				}
-			} else if text == "" && inputForm.GetFormItemIndex(GetVideoQualityDropdownLabel()) != -1 {
-				// Remove the Video Quality dropdown when no text is present in the input field
-				// and atleast 1 existing input dropdown is already exist.
-				inputForm.RemoveFormItem(inputForm.GetFormItemIndex(GetVideoQualityDropdownLabel()))
-
-				// Clear previous status textviews if found any
-				for inputForm.GetFormItemIndex(GetStatusLabel()) != -1 {
-					inputForm.RemoveFormItem(inputForm.GetFormItemIndex(GetStatusLabel()))
-				}
-
-				// Remove download button when no text is present.
-				if inputForm.GetButtonIndex(GetDownloadButtonLabel()) != -1 {
-					inputForm.RemoveButton(inputForm.GetButtonIndex(GetDownloadButtonLabel()))
-				}
+			} else if urlText == "" {
+				// Clear controls if already exist
+				removeControls(inputForm, true, true, true, true)
 			}
 		})
 
@@ -141,14 +178,14 @@ func GetInputForm(application *tview.Application, youtubeVideoOperations *YouTub
 }
 
 // Create & return info form
-func GetInfoForm(application *tview.Application, youtubeVideoOperations *YouTubeVideoOperations) *tview.Form {
+func getInfoForm() *tview.Form {
 	infoForm := tview.NewForm()
 
 	// Add info about how to use the app
 	infoForm = infoForm.AddTextView(
 		GetUsageInfoLabel(),
 		GetUsageInfoText(),
-		0, 1, true, true)
+		0, 3, true, true)
 
 	// Add info about quitting the app
 	infoForm = infoForm.AddTextView(
@@ -164,4 +201,39 @@ func GetInfoForm(application *tview.Application, youtubeVideoOperations *YouTube
 
 	infoForm.SetBorder(true).SetTitle(GetAboutAppTitle()).SetTitleAlign(tview.AlignCenter)
 	return infoForm
+}
+
+// Clears the controls specified by boolean parameters
+func removeControls(
+	inputForm *tview.Form,
+	removeVideoDropdown, removeAudioDropdown, removeStatusTextView, removeDownloadButton bool) {
+	// Remove the Video dropdown when no text is present in the input field
+	// and atleast 1 existing input dropdown is already exist.
+	if removeVideoDropdown {
+		for inputForm.GetFormItemIndex(GetVideoQuality_FPS_Size_Type_DropdownLabel()) != -1 {
+			inputForm.RemoveFormItem(inputForm.GetFormItemIndex(GetVideoQuality_FPS_Size_Type_DropdownLabel()))
+		}
+	}
+
+	// Remove the Audio dropdown when no text is present in the input field
+	// and atleast 1 existing input dropdown is already exist.
+	if removeAudioDropdown {
+		for inputForm.GetFormItemIndex(GetAudioBitrate_Size_Type_DropdownLabel()) != -1 {
+			inputForm.RemoveFormItem(inputForm.GetFormItemIndex(GetAudioBitrate_Size_Type_DropdownLabel()))
+		}
+	}
+
+	// Clear previous status textviews if found any
+	if removeStatusTextView {
+		for inputForm.GetFormItemIndex(GetStatusLabel()) != -1 {
+			inputForm.RemoveFormItem(inputForm.GetFormItemIndex(GetStatusLabel()))
+		}
+	}
+
+	// Remove download button when no text is present.
+	if removeDownloadButton {
+		for inputForm.GetButtonIndex(GetDownloadButtonLabel()) != -1 {
+			inputForm.RemoveButton(inputForm.GetButtonIndex(GetDownloadButtonLabel()))
+		}
+	}
 }
